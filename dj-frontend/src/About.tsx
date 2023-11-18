c76c63cf7a03b0f3f8b85447c1eadc33629c8721
Darref <jordan.ferrad@gmail.com>
2023-11-18 06:13:31 +0100

    perfectly responsive

--- dj-frontend/src/About.tsx
+++ dj-frontend/src/About.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import React from 'react';
+import MenuBar from './MenuBar';
 
 
 function About() {
@@ -7,6 +8,7 @@ function About() {
 
     return (
       <>
+        <MenuBar></MenuBar>
        <div>
           <h1>About</h1>
       </div>
